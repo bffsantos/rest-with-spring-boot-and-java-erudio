d598391fd33d99bcabc7e8705150d496c20f8515
bffsantos <brunoffsantos_2010@hotmail.com>
2025-01-16 16:00:32 -0300

    Configurando Cross-Origin Resource Sharing(CORS)

--- 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ 05_FirstStepsInJavawithSpringBoot/rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -46,6 +47,7 @@ public class PersonController {
 		return services.findAll();
 	}
 	
+	@CrossOrigin(origins = "http://localhost:8080")
 	@GetMapping(value ="/{id}", produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
 	@Operation(summary = "Finds a Person", description = "Finds a Person", tags = {"People"}, 
 	responses = {
@@ -62,6 +64,7 @@ public class PersonController {
 		return services.findById(id);
 	}
 	
+	@CrossOrigin(origins = {"http://localhost:8080", "https://erudio.com.br"})
 	@PostMapping(consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML }, produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML })
 	@Operation(summary = "Adds a new Person", description = "Adds a new Person by passing in a JSON, XML or YML representation of the person.", 
 	tags = {"People"}, 
